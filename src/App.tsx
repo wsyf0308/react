a87a7e6e7204010435851893e89d1fc387014c34
York.Yang <York.Yang@mspbots.ai>
2022-07-27 18:56:45 +0800

    menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,57 @@
-import { useState } from 'react'
+import { JSXElementConstructor, ReactElement, ReactFragment, ReactPortal, useState} from 'react'
 import './App.css'
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 // @ts-ignore
 import Router from './router/index'
-console.log(Router)
+import {Button} from 'antd';
+
 function App() {
   const [count, setCount] = useState(0)
-
+  const Arr = [
+    {
+      name: '一级菜单',
+      children: [
+        {name: '二级菜单'},
+        {name: '二级菜单'},
+        {
+          name: '二级菜单',
+          children: [
+            {name: '三级菜单'},
+            {name: '三级菜单'},
+            {name: '三级菜单'},
+            {
+              name: '四级菜单',
+              children: [
+                {name: '五级菜单'},
+                {name: '五级菜单'},
+                {name: '五级菜单'},
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+  const ItemLi = (item:any) => {
+    return (<li>
+      {item.name}
+      {item.children ? ulMenu(item.children) : null}
+    </li>)
+  }
+  const ulMenu = (arr: any) => {
+    return (<ul>
+      {arr.map((item: any) => {
+         return ItemLi(item)
+      })}
+    </ul>)
+  }
   return (
-      <BrowserRouter>
-          <h1>Welcome to React Router!</h1>
-          <Router />
-      </BrowserRouter>
+    <BrowserRouter>
+      <h1>Welcome to Router!</h1>
+      {ulMenu(Arr)}
+      <Button type="primary">Button</Button>
+      <Router/>
+    </BrowserRouter>
   )
 }
 
